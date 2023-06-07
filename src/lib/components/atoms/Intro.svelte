c4e7a15c7d2482640bdbfee52b401e68de06bf15
Aunali321 <aunvakil.aa@gmail.com>
2023-06-07 05:43:25 +0530

    feat: conatct icons

--- src/lib/components/atoms/Intro.svelte
+++ src/lib/components/atoms/Intro.svelte
@@ -1,4 +1,8 @@
-<div class="intro text-center md:text-left px-8">
+<script>
+	import { A, Li, List } from 'flowbite-svelte';
+</script>
+
+<div class="intro text-center md:text-left mx-5">
 	<!-- <Avatar class="mb-2 inline-block h-10 w-10" src={personal_logo} alt="Me" /> -->
 	<h5 class="font-semilight mb-1 tracking-tight text-slate-600 dark:text-slate-300">
 		Hi, my name is
@@ -20,11 +24,41 @@
 	<h3 class="text-lg font-medium tracking-tight text-slate-600 dark:text-slate-200">
 		Computer Science Student
 	</h3>
+	<List class="flex flex-row justify-center items-center md:justify-start mt-4 space-x-4 list-none">
+		<Li class="flex justify-center">
+			<A href="mailto:aunvakil.aa@gmail.com">
+				<svg
+					class="text-slate-600 dark:text-slate-300 text-2xl h-7 w-7"
+					aria-hidden="true"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="1.5"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</A>
+		</Li>
+		<Li>
+			<A href="https://github.com/Aunali321">
+				<i class="fa-brands fa-github text-slate-600 dark:text-slate-300 text-2xl" />
+			</A>
+		</Li>
+		<Li>
+			<A href="https://discord.com/users/164673069212893184">
+				<i class="fa-brands fa-discord text-slate-600 dark:text-slate-300 text-2xl" />
+			</A>
+		</Li>
+	</List>
 	<p class="mt-4 text-lg font-normal tracking-tight text-slate-500 dark:text-slate-200">
 		A open-source developer from India. I'm a self-taught software developer who's very passionate
 		about reverse engineering.
 	</p>
-	<!-- about. smaller fonts -->
 	<p class="mt-4 text-md tracking-tight text-slate-500 dark:text-slate-200">
 		My interests include Reverse Engineering, P2P File transfer protocol, Web scraping, Bash
 		Scripting, Automation.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,13 @@
 <html lang="en">
 	<head>
 		<title>Aunali K.</title>
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+			integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+			crossorigin="anonymous"
+			referrerpolicy="no-referrer"
+		/>
 	</head>
 	<body class="dark:bg-gray-900">
 		<Navbar navClass="md:px-60 py-3 px-6 md:py-6" let:hidden let:toggle>
